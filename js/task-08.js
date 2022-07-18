1e8c5710e9d0f68d47e2a802c8fc969334343c35
wombatstar <toxaf13@gmail.com>
2022-07-18 21:03:26 +0200

    4

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
-const
+const form = document.querySelector("form.login-form");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const { elements: { email, password } } = e.currentTarget;
+    
+    if (email.value === "" || password.value === "") {
+        return alert("Wypełnij wszystkie pola formularza");
+    };
+
+    const userData = { email: email.value, password: password.value }
+    console.log (userData);
+    e.currentTarget.reset();
+});
