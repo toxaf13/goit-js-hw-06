678c91bb0aaf303e8d84e0e9d5114cfd20cdbb02
wombatstar <toxaf13@gmail.com>
2022-07-19 17:25:32 +0200

    5

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+const background = document.querySelector("span.color");
+const btn = document.querySelector("button.change-color");
+const body = document.querySelector("body");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+btn.addEventListener("click",e =>{
+   body.style.backgroundColor = `${getRandomHexColor()}`;
+   colorName.textContent = `${getRandomHexColor()}`;
+})
+
+
+
+
+
+slider.addEventListener("input", (event) => {
+    text.style.fontSize = slider.value +"px"
+});
